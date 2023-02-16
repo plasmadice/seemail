2e2fc77264e352be0806fca363b9cfa3da5b9e99
Haku <phoenixquint@gmail.com>
2023-02-15 23:57:27 -0500

    Cleanup on main page

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>Some content</p>
+      <h1>Some content</h1>
+      <h4>More content </h4>
       <Link href="/code">Retrieve code from email</Link>
-
+      <br />
+      <p>(unfinished)PIN LOGIN</p> 
+      <a target="_blank" href="https://github.com/plasmadice/seemail">Link to github repo</a>
     </div>
   );
 }
