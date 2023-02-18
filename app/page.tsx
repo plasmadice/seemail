7e0d16898bf03afe191aa2f294a48a36ab28f728
Haku <phoenixquint@gmail.com>
2023-02-17 21:28:42 -0500

    Split page in two with grid

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,11 @@
 import Link from 'next/link'
 import { Grid, Card, Text } from '@nextui-org/react';
 
-const MockItem = ({ text }: any) => {
+const MockItem = ({ text, children }: any) => {
   return (
-    <Card css={{ h: "$20", $$cardColor: '$colors$primary' }}>
+    <Card css={{ h: "$100", $$cardColor: '$colors$primary' }}>
       <Card.Body>
-        <Text h6 size={15} color="white" css={{ m: 0 }}>
-          {text}
-        </Text>
+          {text ? <Text h6 size={15} color="white" css={{ m: 0 }}>{text}</Text> : children}
       </Card.Body>
     </Card>
   );
@@ -16,17 +14,19 @@ const MockItem = ({ text }: any) => {
 
 export default function Home() {
   return (
-    <Grid.Container gap={2} justify="center">
-      <Grid xs={12}>
-        <h1>Some content</h1>
-        <h4>More content </h4>
-        <Link href="/code">Retrieve code from email</Link>
-        <MockItem text={"1 of 2"} />
+    <Grid.Container style={{height: "50vh"}} gap={2} justify="center">
+      <Grid xs={12} style={{height: "100%"}}>
+        <MockItem>
+          <h1>Some content</h1>
+          <h4>More content </h4>
+          <Link href="/code">Retrieve code from email</Link>
+        </MockItem>
       </Grid>
-      <Grid xs={12}>
-        <p>(unfinished)PIN LOGIN</p> 
-        <a target="_blank" href="https://github.com/plasmadice/seemail">Link to github repo</a>
-        <MockItem text={"2 of 2"} />
+      <Grid xs={12} style={{height: "100%"}}>
+        <MockItem>
+          <p>(unfinished)PIN LOGIN</p> 
+          <a target="_blank" href="https://github.com/plasmadice/seemail">Link to github repo</a>
+        </MockItem>
       </Grid>
     </Grid.Container>
   );
