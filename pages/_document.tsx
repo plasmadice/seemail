133c311892f8998682330038c18c9edc3ccad001
Haku <phoenixquint@gmail.com>
2023-02-20 23:35:39 -0500

    Removed unused dependency

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { CssBaseline } from '@nextui-org/react';
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: any) {
     const initialProps = await Document.getInitialProps(ctx);
     return {
       ...initialProps,
-      styles: React.Children.toArray([initialProps.styles])
+      styles: React.Children.toArray([initialProps.styles]),
     };
   }
 
   render() {
     return (
       <Html lang="en">
-        <Head>{CssBaseline.flush()}</Head>
         <body>
           <Main />
           <NextScript />
