81c8038fad5c0f87c86e88463a246729b3d49c17
Haku <phoenixquint@gmail.com>
2023-02-22 18:53:47 -0500

    Color changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="max-w-md max-h-max m-auto grid grid-cols-12 bg-inherit ">
-      <div className="col-span-12 rounded-t bg-gray-600 p-32">
+      <div className="col-span-12 rounded-t bg-rhino p-32">
         <Link href="/code">Retrieve newest code from email</Link>
       </div>
-      <div className="col-span-12 rounded-b border border-gray-500 bg-gray-200 p-32">
+      <div className="col-span-12 rounded-b bg-faded-blue p-32">
         <p>(unfinished)PIN LOGIN</p>
         <a target="_blank" href="https://github.com/plasmadice/seemail">
           Link to github repo
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}'],
   theme: {
     typography: (theme) => ({}),
-    extend: {},
+    extend: {
+      colors: {
+        'firefly': '#152a38',
+        'faded-blue': '#7a86b6',
+        'rhino': '#29435c',
+        'blue-haze': '#c8b6e2',
+      }
+    },
   },
   variants: {},
   plugins: [require('@tailwindcss/typography')],
