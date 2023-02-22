c0c45b3755034b2571e1c4045dab9d1c0b62f025
Haku <phoenixquint@gmail.com>
2023-02-21 21:32:43 -0500

    Use Vercel environment variables for fetch

--- app/code/page.tsx
+++ app/code/page.tsx
@@ -1,6 +1,10 @@
 export default async function CodePage() {
   async function getCode() {
-    const url = `${process.env.BASE_URL}/api/getEmailContents`;
+    const url = `${
+      process.env.NODE_ENV.includes("dev")
+        ? process.env.BASE_URL
+        : process.env.VERCEL_URL
+    }/api/getEmailContents`;
     const res = await fetch(url, { cache: "no-store" });
     const data = await res.json();
     return data?.code;
