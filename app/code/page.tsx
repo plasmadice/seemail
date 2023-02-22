bfaad5381a0c3cbbaf612300dd8cac6909a13b23
Haku <phoenixquint@gmail.com>
2023-02-21 21:52:11 -0500

    Testing process.env.NEXT_PUBLIC_SITE_URL

--- app/code/page.tsx
+++ app/code/page.tsx
@@ -1,14 +1,9 @@
 export default async function CodePage() {
   async function getCode() {
-    console.log(
-      `process.env.NODE_ENV.includes("dev"): ${process.env.NODE_ENV.includes(
-        "dev"
-      )}`
-    );
     const url = `${
       process.env.NODE_ENV.includes("dev")
         ? process.env.BASE_URL
-        : process.env.VERCEL_URL
+        : process.env.NEXT_PUBLIC_SITE_URL
     }/api/getEmailContents`;
     const res = await fetch(url, { cache: "no-store" });
     const data = await res.json();
