c9b57906fdbaddac81f648e3a9015522b66102ba
Haku <phoenixquint@gmail.com>
2023-02-23 23:40:24 -0500

    Change to args and bump to v18 of node on vercel

--- pages/api/enterpin.ts
+++ pages/api/enterpin.ts
@@ -18,7 +18,7 @@ async function getOptions(isDev: boolean) {
     };
   } else {
     options = {
-      args: ['--no-sandbox'],
+      args: chrome.args.push('--no-sandbox'),
       defaultViewport: chrome.defaultViewport,
       executablePath: await chrome.executablePath,
       headless: chrome.headless,
