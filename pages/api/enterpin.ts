77bfbec963c99189bca49f67f095a700e87bc303
Haku <phoenixquint@gmail.com>
2023-02-23 23:35:02 -0500

    Custom args

--- pages/api/enterpin.ts
+++ pages/api/enterpin.ts
@@ -18,7 +18,7 @@ async function getOptions(isDev: boolean) {
     };
   } else {
     options = {
-      args: chrome.args,
+      args: ['--no-sandbox'],
       defaultViewport: chrome.defaultViewport,
       executablePath: await chrome.executablePath,
       headless: chrome.headless,
