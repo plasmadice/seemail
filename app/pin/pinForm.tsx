4979fd8bb2ce8d81ee38b6009af0a2ef449cad04
Haku <phoenixquint@gmail.com>
2023-03-29 23:37:04 -0400

    Moved enterpin func to gcp functions and prep for screenshot demo mode

--- app/pin/pinForm.tsx
+++ app/pin/pinForm.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { useState, useEffect } from "react";
+import { Tooltip } from "react-tooltip";
 
 export default function PinForm() {
   type apiResponse = {
     status: number;
     body: string;
     error: string | undefined;
+    // imageStr: string | undefined;
   };
 
   const [text, setText] = useState("");
@@ -13,8 +15,10 @@ export default function PinForm() {
     status: 0,
     body: "",
     error: "",
+    // imageStr: ""
   });
   const [waiting, setWaiting] = useState(false);
+  const [screenshotChecked, setScreenshotChecked] = useState(false);
 
   const Spinner = () => {
     const [count, setCount] = useState(60);
@@ -50,9 +54,15 @@ export default function PinForm() {
 
     const res = await fetch(url, { cache: "no-store" });
     const status = res.status;
+    console.log(`res.status: ${res.status}`);
     const data: apiResponse = await res.json();
     const body = data.body;
+    // const imageStr = data.body.imageStr
+    console.log(`typeof data.body: ${typeof data.body}`);
+    console.log(`body: ${body}`);
+    console.log(`typeof data.body: ${typeof data.body}`);
     const error = data?.error;
+    // if (console.log())
     setResponse({ body, status, error });
     await handleClearInput();
   }
@@ -66,13 +76,20 @@ export default function PinForm() {
     setText(e.target.value);
   }
 
+  async function handleScreenshotMode() {
+    setScreenshotChecked((prev) => {
+      !prev ? setText("123456") : setText("");
+      return !prev;
+    });
+  }
+
   return (
     <form
       onSubmit={sendPin}
       noValidate
-      className="w-[90%] h-full grid grid-cols-1"
+      className="w-[90%] h-full grid grid-cols-1 grid-rows-6"
     >
-      <div className="h-[70%][ space-y-4] grid grid-cols-1">
+      <div className="h-[70%][ space-y-4] grid grid-cols-1 row-span-3">
         <p className="text-white font-mono text-base pt-4">PIN LOGIN</p>
         <input
           type="text"
@@ -81,10 +98,10 @@ export default function PinForm() {
           placeholder='"123456"'
           className="w-full max-h-10 appearance-none bg-blue-haze border-none text-gray-700 py-1 px-2 leading-tight focus:outline"
         />
-        <div className="w-full grid grid-cols-2 place-content-between px-4 gap-10">
+        <div className="w-full grid grid-cols-6 m-auto content-center gap-4 px-2">
           <button
             onClick={sendPin}
-            className="flex-shrink-0 bg-blue-800 hover:bg-firefly text-sm text-white py-1 px-2 rounded row-span-1"
+            className="flex-shrink-0 bg-blue-800 hover:bg-firefly text-sm text-white py-1 px-2 rounded col-span-3 row-span-1"
             type="button"
           >
             Submit
@@ -92,14 +109,35 @@ export default function PinForm() {
           <button
             disabled={waiting}
             onClick={handleClearInput}
-            className="flex-shrink-0 bg-persian-red hover:bg-firefly text-sm text-white py-1 px-2 rounded row-span-1"
+            className="flex-shrink-0 bg-persian-red hover:bg-firefly text-sm text-white py-1 px-2 rounded col-span-3"
             type="button"
           >
             Clear
           </button>
         </div>
       </div>
-      <div className="w-full h-full grid grid-cols-1 space-y-1 place-content-around">
+      <div
+        data-tooltip-id="tooltip-text"
+        data-tooltip-content="CURRENTLY DISABLED: Enables screenshot mode: Sends screenshots of progress from the server"
+        className="w-full grid grid-cols-12 row-span-1 place-content-center"
+      >
+        <label
+          onClick={handleScreenshotMode}
+          htmlFor="screenshotMode"
+          className="col-span-4 text-white"
+        >
+          Demo Mode
+        </label>
+        <input
+          onChange={handleScreenshotMode}
+          className="col-span-1 h-6 w-6 place-self-end"
+          name="screenshotMode"
+          type="checkbox"
+          checked={screenshotChecked}
+        />
+      </div>
+      <div className="w-full grid grid-cols-1 row-span-2 space-y-1 place-content-around">
+        <Tooltip id="tooltip-text" className="text-white h-0" />
         <span className="text-goldenrod text-lg font-medium italic">
           {waiting ? "Usually takes 30-50 seconds" : response.body}
         </span>
--- pages/api/enterpin.ts
+++ pages/api/enterpin.ts
@@ -1,14 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import chromium from "@sparticuz/chromium-min"
-import puppeteer from "puppeteer-core"
-
-const exePath =
-  process.platform === "win32"
-    ? "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-    : process.platform === "linux"
-    ? "/usr/bin/google-chrome"
-    : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-
 
 export default async function handler (
   req: NextApiRequest,
@@ -16,130 +6,24 @@ export default async function handler (
 ) {
   
   const main = async () => {
+    console.log(`Hitting new endpoint`)
 
-    const pageUrl: any = process.env.SITE_URL
-    const email: any = process.env.SITE_EMAIL
-    const passWord: any = process.env.SITE_PASSWORD
+    // const pageUrl: any = process.env.SITE_URL
   
     const pin: any = req?.query?.pin
+    const screenshots: any = req?.query?.screenshots || false
 
-    const screenshot = async (page: any) => {
+    console.log(`pin: ${pin}`)
+    console.log(`screenshots: ${screenshots}`)
 
-      console.log("Taking screenshot for debugging. Expect header errors.")
-      res.statusCode = 200
-      res.setHeader("Content-Type", `image/png`)
-      const file = await page.screenshot({
-        type: "png",
-      })
-
-      res.send(file)
-    }
-
-    const returnHTML = async (page: any) => {
-      
-      const html = await page.content()
-      console.log(html)
-
-      console.log("Returning HTML for debugging. Expect header errors.")
-      res.statusCode = 200
-      res.send(html)
-    }
-
-    const getCode = async () => {
-      const url = `${process.env.NEXT_PUBLIC_URL}/api/getEmailContents`
-      const res = await fetch(url, { cache: "no-store" })
-      const data = await res.json()
-      const code = data.code
-      return code
-    }
-
-    try { /* to open the browser */
-
-      // pin length sanity check
-      if (!pin || pin.length < 6 || pin.length > 10 || Number.isNaN(Math.abs(pin.replaceAll("-","").replaceAll(" ","")))) {
-        console.log("Pin failed validation")
-
-        res.status(400).json({body: "PIN must be a 6-digit number"})
-
-      } else {
-    
-        // launch a new headless browser with dev / prod options
-        const browser = await puppeteer.launch({
-          args: process.env.NODE_ENV.includes('development') ? [] : chromium.args,
-          defaultViewport: process.env.NODE_ENV.includes('development') ? undefined : chromium.defaultViewport,
-          executablePath: process.env.NODE_ENV.includes('development') ? exePath : await chromium.executablePath(
-            "https://github.com/plasmadice/seemail/releases/download/v110.0.1-alpha/chromium-v110.0.1-pack.tar"
-          ),
-          headless: process.env.NODE_ENV.includes('development') ? true : chromium.headless,
-          ignoreHTTPSErrors: process.env.NODE_ENV.includes('development') ? undefined : true,
-        })
-        
-        const page = await browser.newPage()
-
-        try { /* to navigate and login */
-        
-          await page.goto(pageUrl, { waitUntil: "networkidle0" })
-    
-          // Login
-          await page.type(".form-control[name='email']", email)
-          await page.type(".form-control[name='password']", passWord)
-          await page.click('.btn-login')
-
-          await page.waitForNetworkIdle({ idleTime: 2000 })
-
-          // Waits for activation code page after login (common in cloud environment)
-          const activationCodeBox = await page.$('input.form-control[placeholder="Activation code"]')
-
-          if (activationCodeBox) {
-            await page.type('input.form-control[placeholder="Activation code"]', await getCode())
-            await page.keyboard.press('Enter')
-            await page.waitForNetworkIdle({ idleTime: 2000 })
-          }
-          
-          await page.waitForSelector('.ng-scope > .profile:not(.disabled)  > .avatar > img[src="/assets/img/user_header_avatar.png"]'),
-          await page.click('.ng-scope > .profile:not(.disabled)  > .avatar > img[src="/assets/img/user_header_avatar.png"]')
-          
-          await page.waitForNetworkIdle({ idleTime: 2000 })
-      
-          // Waits for bank checkmark after login and handles it
-          const reminderBox = await page.$('.conform-checkbox')
-          reminderBox ?  await page.click('.conform-checkbox') : null
-          const reminderButton = await page.$('.btn-success:not(.w-full)')
-          reminderButton ?  await page.click('.btn-success:not(.w-full)') : null
-      
-          // Navigate to pin login
-          await page.goto(`${process.env.SITE_URL}/account/pin`)
-          await page.waitForNetworkIdle({ idleTime: 1000 })
-          
-          // Enter pin
-          await page.type('input.pin', pin)
-          await page.click('button.btn-primary')
-          
-          await page.waitForNetworkIdle({ idleTime: 1000 })
-          const successResponse = await page.$('div.authorized-pin')
-
-          // close the browser
-          await browser.close()
-
-          const error = successResponse ? undefined : "This is a bad thing."
-          const msg = successResponse ? `Device with PIN ${pin} is authorized` : "Authorization PIN not found"
-
-          res.status(200).json({ body: msg, error })
-        } catch (e: any) {
-          res.status(400).json({
-            body: "Failure AFTER browser creation",
-            error: e.message
-          })
-        }
-      }
-    } catch (e: any) {
-      res.status(400).json({
-        body: "Sorry, Something went wrong!",
-        error: e.message
-      })
-    }
+    const url = `${process.env.GCP_ENTERPIN_URL}/?pin=${pin}${screenshots ? '&screenshots=true' : ''}`
+    console.log(`typeof url: ${typeof url}`)
+    const result = await fetch(url, { cache: "no-store" })
+    const data = await result.json()
+    res.status(200).send(data)
   }
   
   // main() 
   return main().catch(console.log)
 }
+1
