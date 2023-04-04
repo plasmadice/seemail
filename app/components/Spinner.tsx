978ab8eaa2f581daa68730d52cf1c9ae79098462
Haku <phoenixquint@gmail.com>
2023-04-03 21:51:59 -0400

    Add waiting text

--- app/components/Spinner.tsx
+++ app/components/Spinner.tsx
@@ -13,14 +13,17 @@ export default function Spinner({ duration = 90 }) {
   }, [])
 
   return (
-    <div className='z-30 flex items-center justify-center'>
+    <div className='z-30 grid grid-cols-1 justify-items-center'>
       <div
-        className='text-warning relative z-0 h-10 w-10 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]'
+        className='text-warning absolute z-30 h-10 w-10 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]'
         role='status'
       />
 
-      <p className='absolute flex h-8 w-8 items-center justify-center text-white'>
+      <p className='relative flex h-10 w-10 items-center justify-center text-white'>
         {count > 0 ? count : 0}
+      </p>
+      <p className='relative z-30 h-8 items-center justify-center font-medium text-white'>
+        30-60 seconds later...
       </p>
     </div>
   )
