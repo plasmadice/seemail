99e9dca51cc550e61f992b0d58c356b323f53176
plasmadice <herman.white.third@gmail.com>
2023-04-24 14:01:13 -0400

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/react'
+
 import './globals.css'
 
 export const metadata = {
@@ -20,6 +22,7 @@ export default function RootLayout({
     <html className='dark' lang='en'>
       <body className='flex h-screen place-items-center items-center overflow-hidden bg-white text-slate-500 antialiased dark:bg-firefly dark:text-slate-400'>
         {children}
+        <Analytics />
       </body>
     </html>
   )
