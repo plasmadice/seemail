a14a789e9430061b5065998bfb2641f3b435325d
plasmadice <19928793+plasmadice@users.noreply.github.com>
2024-04-14 16:06:00 -0400

    Changed from AUTH_PASS to APP_PASS and fixed issue with /code page

--- app/api/getEmailContents/route.ts
+++ app/api/getEmailContents/route.ts
@@ -1,49 +1,51 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { ImapFlow } from "imapflow"
+const { ImapFlow } = require("imapflow")
 import { NextResponse } from "next/server"
-
-export const revalidate = 0
 
 export async function GET(request: Request) {
   const client = new ImapFlow({
-    host: process.env.HOST as string,
-    port: process.env.PORT as unknown as number,
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     secure: true,
     auth: {
-      user: process.env.AUTH_USER as string,
-      pass: process.env.AUTH_PASS,
+      user: process.env.AUTH_USER,
+      pass: process.env.APP_PASS,
     },
     logger: false,
   })
 
-  let data = { code: "123456", date: "2021-08-01T00:00:00.000Z" }
+  let status, data = {}
 
   const main = async () => {
     // Wait until client connects and authorizes
     await client.connect()
 
-    // // Select and lock a mailbox. Throws if mailbox does not exist
+    // Select and lock a mailbox. Throws if mailbox does not exist
     let lock = await client.getMailboxLock("INBOX")
 
     try {
       let list = await client.search({ subject: process.env.DEFAULT_SEARCH })
 
-      let lastMsg: any = list[list.length - 1]
+      let lastMsg = list[list.length - 1]
 
       let msg = await client.fetchOne(lastMsg, { envelope: true, source: true })
       const indexOfBold = msg.source.toString().indexOf("bold;'")
       let rest = msg.source.toString().slice(indexOfBold + 7)
       const code = rest.split("</p>")[0]
-      const date: any = msg.envelope.date
+      const date = msg.envelope.date
+
+      status = 200
       data = { code, date }
+    } catch (e) {
+      console.log(e)
+      status = 500
+      data = { error: e }
     } finally {
       // Make sure lock is released, otherwise next `getMailboxLock()` never returns
       lock.release()
     }
-
-    await client.logout()
   }
 
   await main()
-  return NextResponse.json(data)
+  await client.logout()
+  return NextResponse.json(data, { status })
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,20 @@ import PinForm from "./pin/pinForm"
 import Footer from "./components/Footer"
 
 export default function Home() {
+
   return (
     <div className="m-auto flex max-h-screen flex-col">
       <div className="p-8 bg-neutral flex h-auto w-fit flex-col place-content-start items-center rounded-lg text-center">
-        <p className="text-neutral-content font-bold">Check Email: <Link
-          className="link pt-4 text-neutral-content hover:underline"
-          href="/code"
-          prefetch={false}
-        >
-          Retrieve newest code
-        </Link></p>
+        <p className="text-neutral-content font-bold">
+          Check Email:
+          <Link
+            className="link pt-4 text-neutral-content hover:underline"
+            href="/code"
+            prefetch={false}
+          >
+            Retrieve newest code
+          </Link>
+        </p>
         <PinForm />
         <Footer />
       </div>
