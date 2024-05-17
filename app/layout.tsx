2e952fea2da55a19be6c49d01883a25fc26ca724
plasmadice <19928793+plasmadice@users.noreply.github.com>
2024-05-17 01:25:00 -0400

    Added user authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,12 @@
 import { Analytics } from "@vercel/analytics/react"
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  ClerkLoaded,
+  UserButton
+} from '@clerk/nextjs'
 
 import "./globals.css"
 
@@ -19,11 +27,30 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html className="dark" lang="en">
-      <body className="text-primary-content bg-base-100 flex h-screen place-items-center items-center overflow-hidden antialiased">
-        {children}
-        <Analytics />
-      </body>
-    </html>
+    <ClerkProvider>
+      <html className="dark" lang="en">
+        <body className="text-primary-content bg-base-100 flex h-screen place-items-center items-center overflow-hidden antialiased">
+          <ClerkLoaded>
+            <SignedIn>
+              <div className="flex flex-col gap-4 items-center mx-auto">
+                <UserButton />
+                {children}
+              </div>
+            </SignedIn>
+            <SignedOut>
+              <div className="p-4 m-auto justify-center rounded-md flex flex-col gap-2 items-center bg-secondary text-secondary-content">
+                <h1 className="text-3xl font-semibold">Seemail</h1>
+                <p className="text-center text-lg">Automate adding authorized devices to your account.</p>
+                <p className="text-center text-lg">Invite only</p>
+                <div className="p-2 px-4 mt-2 bg-secondary-focus rounded-md">
+                  <SignInButton />
+                </div>
+              </div>
+            </SignedOut>
+            <Analytics />
+          </ClerkLoaded>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,42 @@
 import Link from "next/link"
 import PinForm from "./pin/pinForm"
 import Footer from "./components/Footer"
+import { useUser } from "@clerk/nextjs";
 
 export default function Home() {
+  const { user } = useUser()
 
-  return (
-    <div className="m-auto flex max-h-screen flex-col">
-      <div className="p-8 bg-neutral flex h-auto w-fit flex-col place-content-start items-center rounded-lg text-center">
-        <p className="text-neutral-content font-bold">
-          Check Email:
-          <Link
-            className="link pt-4 text-neutral-content hover:underline"
-            href="/code"
-            prefetch={false}
-          >
-            Retrieve newest code
-          </Link>
-        </p>
-        <PinForm />
-        <Footer />
+  const username = user?.username
+
+  if (username?.toLowerCase()?.includes(process.env.NEXT_PUBLIC_CCODE as string)) {
+    return (
+      <div className="m-auto flex max-h-screen flex-col">
+        <div className="p-8 bg-neutral flex h-auto w-fit flex-col place-content-start items-center rounded-lg text-center">
+          <p className="text-neutral-content font-bold">
+            Check Email:
+            <Link
+              className="link pt-4 text-neutral-content hover:underline"
+              href="/code"
+              prefetch={false}
+            >
+              Retrieve newest code
+            </Link>
+          </p>
+          <PinForm />
+          <Footer />
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div className="m-auto flex max-h-screen flex-col">
+        <div className="p-8 bg-neutral flex h-auto w-fit flex-col place-content-start items-center rounded-lg text-center">
+          <p className="text-neutral-content font-bold">
+            Not using an authorized account
+          </p>
+          <Footer />
+        </div>
+      </div>
+    )
+  }
 }
