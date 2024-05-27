799357aab1e5e28a63650e449324c6bd7f018703
plasmadice <19928793+plasmadice@users.noreply.github.com>
2024-05-26 21:56:20 -0400

    Fixed matcher in middleware and re-route logged out users to signin

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
-// import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react"
 import {
   ClerkProvider,
-  SignInButton,
-  SignedIn,
   SignedOut,
-  ClerkLoaded,
-  UserButton
+  UserButton,
+  RedirectToSignIn,
+  ClerkLoaded
 } from '@clerk/nextjs'
 
 import "./globals.css"
@@ -30,27 +29,18 @@ export default function RootLayout({
     <ClerkProvider>
       <html className="dark" lang="en">
         <body className="text-primary-content bg-base-100 flex h-screen place-items-center items-center overflow-hidden antialiased">
-          <ClerkLoaded>
-            <SignedIn>
+            <ClerkLoaded>
               <div className="flex flex-col gap-4 items-center mx-auto">
                 <UserButton />
                 {children}
               </div>
-            </SignedIn>
-            <SignedOut>
-              <div className="p-4 m-auto justify-center rounded-md flex flex-col gap-2 items-center bg-secondary text-secondary-content">
-                <h1 className="text-3xl font-semibold">Seemail</h1>
-                <p className="text-center text-lg">Automate adding authorized devices to your account.</p>
-                <p className="text-center text-lg">Invite only</p>
-                <div className="p-2 px-4 mt-2 bg-secondary-focus rounded-md">
-                  <SignInButton />
-                </div>
-              </div>
-            </SignedOut>
-            {/* <Analytics /> */}
-          </ClerkLoaded>
+            </ClerkLoaded>
         </body>
       </html>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+      <Analytics />
     </ClerkProvider>
   )
 }
