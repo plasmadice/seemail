a5f8b9b859f751a4c7bb4e39bde468353e7f9ab6
plasmadice <19928793+plasmadice@users.noreply.github.com>
2024-06-22 17:43:02 -0400

    Unfix headers

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 
 const cspHeader = `
-    default-src: 'self'  https://*.clerk.com  https://*.clerk.com/*;
-    script-src: 'self' 'unsafe-inline' 'unsafe-eval' https://clerk.seemail.dev https://*.clerk.accounts.dev https://*.vercel-scripts.com;
-    connect-src: 'self' https://clerk.seemail.dev https://*.clerk.accounts.dev ${process.env.NEXT_PUBLIC_URL.replace('https', 'ws')} ${process.env.NEXT_PUBLIC_URL};
-    worker-src: 'self' blob: ${process.env.NEXT_PUBLIC_URL};
-    style-src: 'self' 'unsafe-inline';
-    img-src: 'self' blob: data: https://img.clerk.com;
-    font-src: 'self';
-    object-src: 'none';
-    base-uri: 'self';
-    form-action: 'self';
-    frame-ancestors: 'none';
-    upgrade-insecure-requests: 1;
+    default-src  https://*.clerk.com  https://*.clerk.com/* 'self';
+    script-src 'self' 'unsafe-inline' 'unsafe-eval' https://clerk.seemail.dev https://*.clerk.accounts.dev https://*.vercel-scripts.com;
+    connect-src 'self' https://clerk.seemail.dev https://*.clerk.accounts.dev ${process.env.NEXT_PUBLIC_URL.replace('https', 'ws')} ${process.env.NEXT_PUBLIC_URL};
+    worker-src 'self' blob: ${process.env.NEXT_PUBLIC_URL};
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data: https://img.clerk.com;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
 `
 
 const nextConfig = {
