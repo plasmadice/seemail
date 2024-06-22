9a1bdd5450aa1fe48656e036c26dafedd8e03e3e
plasmadice <19928793+plasmadice@users.noreply.github.com>
2024-06-22 17:58:47 -0400

    Revert layout file, but move signedout logic into

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { Analytics } from "@vercel/analytics/react"
 import {
   ClerkProvider,
-  SignedIn,
   SignedOut,
   UserButton,
   RedirectToSignIn,
@@ -28,22 +27,20 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <ClerkLoaded>
-        <SignedIn>
-          <html className="dark" lang="en">
-            <body className="text-primary-content bg-base-100 flex h-screen place-items-center items-center overflow-hidden antialiased">
-                  <div className="flex flex-col gap-4 items-center mx-auto">
-                    <UserButton />
-                    {children}
-                  </div>
-            </body>
-          </html>
-        </SignedIn>
-        <SignedOut>
-          <RedirectToSignIn />
-        </SignedOut>
-        <Analytics />
-      </ClerkLoaded>
+      <html className="dark" lang="en">
+        <body className="text-primary-content bg-base-100 flex h-screen place-items-center items-center overflow-hidden antialiased">
+            <ClerkLoaded>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+              <div className="flex flex-col gap-4 items-center mx-auto">
+                <UserButton />
+                {children}
+              </div>
+            </ClerkLoaded>
+        </body>
+      </html>
+      <Analytics />
     </ClerkProvider>
   )
 }
